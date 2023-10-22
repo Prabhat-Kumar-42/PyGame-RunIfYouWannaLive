fa310a3c9edc12dfb1fd44ad835708702073f6df
blaze <prabhatkumarverma42@gmail.com>
2023-10-22 15:46:45 +0530

    Learned about animations and Added snail animation to the game

--- FirstPyGame.py
+++ FirstPyGame.py
@@ -10,9 +10,6 @@ pygame.display.set_caption('Run If You Wanna Live')
 #clock obj, helps with time and controlling framerate
 clock = pygame.time.Clock() 
 
-# creating font object to create a font surface
-surface_font = pygame.font.Font('./font/Pixeltype.ttf', 50) 
-
 width = 800
 height = 400
 screen = pygame.display.set_mode((width, height))
@@ -24,10 +21,17 @@ test_surface_height = 200
 test_surface = pygame.Surface((test_surface_width, test_surface_height)) # create regular surface
 test_surface.fill('White') # fill the surface with a color
 '''
- #code for loading an image to a surface
-sky_surface = pygame.image.load('./graphics/sky.png')
-ground_surface = pygame.image.load('./graphics/ground.png')
-text_surface = surface_font.render('Run If You Wanna Live', False, 'Dark Green')
+
+#code for loading an image to a surface
+sky_surface = pygame.image.load('./graphics/sky.png').convert()
+ground_surface = pygame.image.load('./graphics/ground.png').convert()
+surface_font = pygame.font.Font('./font/Pixeltype.ttf', 50) # creating font object to create a font surface
+text_surface = surface_font.render('Run If You Wanna Live', False, 'Dark Green').convert()
+
+#snail_surface
+snail_surface = pygame.image.load('./graphics/snail/snail1.png').convert_alpha()
+snail_x_pos = 800
+snail_y_pos = 260
 
 while(True):
     for event in pygame.event.get():
@@ -41,6 +45,11 @@ while(True):
     screen.blit(sky_surface , (0,0)) #(surface, position) 
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (250, 100))
+
+    if(snail_x_pos <= -100):
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, snail_y_pos))
+    snail_x_pos = snail_x_pos-4
 
     pygame.display.update() # update the display surface, in our case screen
     clock.tick(60) # set upperlimit for framerate as 60 fps
