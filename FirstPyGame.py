1db42a0206227fc1608ab180aa5dafb9408e6118
blaze <prabhatkumarverma42@gmail.com>
2023-10-22 14:30:11 +0530

    learned how to add regular surface, fill them up with color, load image to regular surface and display them to display surface, learned methods() - pygame.Surface(width, height), pygame.image.load('./path_to_image'), blit() method to display regular surface over display surface

--- FirstPyGame.py
+++ FirstPyGame.py
@@ -8,8 +8,18 @@ clock = pygame.time.Clock() #clock obj, helps with time and controlling framerat
 
 width = 800
 height = 400
- 
 screen = pygame.display.set_mode((width, height))
+
+'''
+---------- Code for creating and adding color to test surface ---------------
+test_surface_width = 100
+test_surface_height = 200
+test_surface = pygame.Surface((test_surface_width, test_surface_height)) # create regular surface
+test_surface.fill('White') # fill the surface with a color
+'''
+ #code for loading an image to a surface
+sky_surface = pygame.image.load('./graphics/Sky.png')
+
 while(True):
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # pygame.QUIT is synonomus to close button(x) button in display screen  
@@ -17,5 +27,9 @@ while(True):
             exit()                    # exit() from sys module closes any kind of code once it's called
     # draw all our elements
     # update everything
+
+    # attach and display the test_surface to display surface, bli stands for block image transfer (fancy way of saying put one surface on another surface)
+    screen.blit(sky_surface , (0,0)) #(surface, position) 
+
     pygame.display.update() # update the display surface, in our case screen
     clock.tick(60) # set upperlimit for framerate as 60 fps
