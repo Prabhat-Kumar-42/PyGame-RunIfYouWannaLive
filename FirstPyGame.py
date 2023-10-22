302b599402ef9aa1ed8d77ea60086a1fc3dfe545
blaze <prabhatkumarverma42@gmail.com>
2023-10-22 20:37:50 +0530

    Added Jump Animations, Added Collision with object, Added Game End condition,Added Score Display, Moved ScreenInitialization to a method, Added Start-Screen(Title-Screen) and Game-Active Screen

--- FirstPyGame.py
+++ FirstPyGame.py
@@ -1,5 +1,21 @@
 import pygame
 from sys import exit
+from pygame.constants import KEYDOWN, MOUSEBUTTONDOWN
+
+def display_score():
+    score_surface = surface_font.render("Score: ", False, "Dark Green")
+    score_rectangle = score_surface.get_rect(midright = (700, 50))
+    screen.blit(score_surface, score_rectangle)
+    current_time = round(pygame.time.get_ticks()/1000, 2)
+    time_surface = surface_font.render(str(current_time), False, "Dark Green")
+    time_rectangle = time_surface.get_rect(midleft = (700, 50))
+    screen.blit(time_surface, time_rectangle)
+
+def initializeScreen():
+    # attach and display the test_surface to display surface, bli stands for block image transfer (fancy way of saying put one surface on another surface)
+    screen.blit(sky_surface , (0,0)) #(surface, position) 
+    screen.blit(ground_surface, (0, 300))
+    
 
 #pygame.init() initializes pygame
 pygame.init() 
@@ -9,6 +25,9 @@ pygame.display.set_caption('Run If You Wanna Live')
 
 #clock obj, helps with time and controlling framerate
 clock = pygame.time.Clock() 
+
+#boolean to check if game is over or not
+game_active = False 
 
 width = 800
 height = 400
@@ -26,8 +45,11 @@ screen = pygame.display.set_mode((width, height))
 #i.e, it speeds up pygame working
 sky_surface = pygame.image.load('./graphics/sky.png').convert()
 ground_surface = pygame.image.load('./graphics/ground.png').convert()
+
+#score_surface
 surface_font = pygame.font.Font('./font/Pixeltype.ttf', 50) # creating font object to create a font surface
-text_surface = surface_font.render('Run If You Wanna Live', False, 'Dark Green').convert()
+title_surface = surface_font.render('Run If You Wanna Live', False, 'Dark Green').convert()
+title_rectangle = title_surface.get_rect(center = (400, 50))
 
 #snail
 snail_surface = pygame.image.load('./graphics/snail/snail1.png').convert_alpha()
@@ -38,27 +60,68 @@ snail_rectangle = snail_surface.get_rect(midbottom = (snail_x_pos,snail_y_pos))
 #player
 player_surface = pygame.image.load('./graphics/Player/player_walk_1.png').convert_alpha()
 player_rectangle = player_surface.get_rect(midbottom= (80, 300)) 
+player_gravity = 0
 
 while(True):
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # pygame.QUIT is synonomus to close button(x) button in display screen  
             pygame.quit()             # pygame.quit() uninitializes pygame, it's basically opposite of pygame.init() 
             exit()                    # exit() from sys module closes any kind of code once it's called
-    # draw all our elements
+        if game_active:
+            if player_rectangle.bottom >= 300:
+                if event.type == pygame.KEYDOWN:
+                    #print('key down')
+                    if event.key == pygame.K_SPACE: 
+                        player_gravity = -20
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    #print('mouse down')
+                    if player_rectangle.collidepoint(event.pos) and player_rectangle.bottom >= 300:
+                        player_gravity = -20
+        else:
+            if((event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE) 
+                or event.type == pygame.MOUSEBUTTONDOWN):
+                game_active = True
+                snail_rectangle.left = 800
+        # draw all our elements
     # update everything
+    
+    if game_active :
+        initializeScreen()   
+        display_score()
 
-    # attach and display the test_surface to display surface, bli stands for block image transfer (fancy way of saying put one surface on another surface)
-    screen.blit(sky_surface , (0,0)) #(surface, position) 
-    screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (250, 100))
+        #pygame.draw.rect(screen, '#c0e8ec', score_rectangle)
+        #pygame.draw.rect(screen, '#c0e8ec', score_rectangle, 1)
 
-    # reset snail position so it re-appears after it reaches it's end position
-    if(snail_rectangle.right < 0):
-        snail_rectangle.left = 800
-    screen.blit(snail_surface, snail_rectangle)
-    snail_rectangle.left = snail_rectangle.left-4 #changes so snail appeares to be moving along x_pos
+        # reset snail position so it re-appears after it reaches it's end position
+        if(snail_rectangle.right < 0):
+            snail_rectangle.left = 800
+        screen.blit(snail_surface, snail_rectangle)
+        snail_rectangle.left = snail_rectangle.left-4 #changes so snail appeares to be moving along x_pos
+    
+        #player
+        player_gravity = player_gravity + 1
+        player_rectangle.y += player_gravity
+        if(player_rectangle.bottom >= 300):
+            player_rectangle.bottom = 300
+        screen.blit(player_surface, player_rectangle)
+   
+        #collision
+        if snail_rectangle.colliderect(player_rectangle):
+            game_active = False
+   #if player_rectangle.colliderect(snail_rectangle):
+    #    print("Collision")
 
-    screen.blit(player_surface, player_rectangle)
-
+    #mouse_pos = pygame.mouse.get_pos()
+    #if ( player_rectangle.collidepoint(mouse_pos)):
+    #    print("collision")
+    else:
+        initializeScreen()
+        screen.blit(title_surface, title_rectangle)
+        player_rectangle.bottom = 300
+        player_rectangle.left = 80
+        screen.blit(player_surface, player_rectangle)
+        pass
+   
     pygame.display.update() # update the display surface, in our case screen
     clock.tick(60) # set upperlimit for framerate as 60 fps
