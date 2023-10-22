9601dedf9ecb7b6ffc722ee4977c8e77f1e939f9
blaze <prabhatkumarverma42@gmail.com>
2023-10-22 16:51:19 +0530

    Updated to it uses rectangles to influence positions

--- FirstPyGame.py
+++ FirstPyGame.py
@@ -29,10 +29,15 @@ ground_surface = pygame.image.load('./graphics/ground.png').convert()
 surface_font = pygame.font.Font('./font/Pixeltype.ttf', 50) # creating font object to create a font surface
 text_surface = surface_font.render('Run If You Wanna Live', False, 'Dark Green').convert()
 
-#snail_surface
+#snail
 snail_surface = pygame.image.load('./graphics/snail/snail1.png').convert_alpha()
 snail_x_pos = 800
-snail_y_pos = 260
+snail_y_pos = 300 
+snail_rectangle = snail_surface.get_rect(midbottom = (snail_x_pos,snail_y_pos))
+
+#player
+player_surface = pygame.image.load('./graphics/Player/player_walk_1.png').convert_alpha()
+player_rectangle = player_surface.get_rect(midbottom= (80, 300)) 
 
 while(True):
     for event in pygame.event.get():
@@ -47,11 +52,13 @@ while(True):
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (250, 100))
 
-    # reset snail_x_pos so it re-appears after it reaches it's end position
-    if(snail_x_pos <= -100):
-        snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, snail_y_pos))
-    snail_x_pos = snail_x_pos-4 #changes so snail appeares to be moving along x_pos
+    # reset snail position so it re-appears after it reaches it's end position
+    if(snail_rectangle.right < 0):
+        snail_rectangle.left = 800
+    screen.blit(snail_surface, snail_rectangle)
+    snail_rectangle.left = snail_rectangle.left-4 #changes so snail appeares to be moving along x_pos
+
+    screen.blit(player_surface, player_rectangle)
 
     pygame.display.update() # update the display surface, in our case screen
     clock.tick(60) # set upperlimit for framerate as 60 fps
